8522ae75af98202018487fae3a3e3b771deb102d
koirherianto <koir.herianto@gmail.com>
2023-08-08 09:04:06 +0800

    pemasangan infyom dan spatie role

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        InfyOm\GeneratorBuilder\GeneratorBuilderServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
 
     /*
